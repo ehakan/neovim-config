a53043a11fd825e3a97bda0f566891079bef6a33
ehakan <ehakangencoglu@gmail.com>
2023-02-03 22:42:51 +0300

    first commit from template

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,12 @@
+return {
+  -- install nordfox via nightfox
+  { "EdenEast/nightfox.nvim" },
+
+  -- configure LazyVim to load nordfox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordfox",
+    },
+  },
+}
