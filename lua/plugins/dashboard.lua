db554b00ca7fe88c709c08192da7dcb8b9b53d12
ehakan <ehakangencoglu@gmail.com>
2023-02-04 18:25:36 +0300

    modify dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, dashboard)
+      local header = [[
+             _           _                 
+         ___| |__   __ _| | ____ _ _ __    
+        / _ \ '_ \ / _` | |/ / _` | '_ \   
+       |  __/ | | | (_| |   < (_| | | | |  
+        \___|_| |_|\__,_|_|\_\__,_|_| |_|  
+                  _ ____   _(_)_ __ ___    
+                 | '_ \ \ / / | '_ ` _ \   
+                _| | | \ V /| | | | | | |  
+               (_)_| |_|\_/ |_|_| |_| |_|  
+      ]]
+      dashboard.section.header.val = vim.split(header, "\n")
+    end,
+  },
+}
