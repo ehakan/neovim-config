413e6118261cdc6d10af33e25e64663a10101e12
ehakan <ehakangencoglu@gmail.com>
2023-02-05 18:12:28 +0300

    move filetype definitions

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.do_filetype_lua = 1
+
+vim.filetype.add({
+  extension = {
+    ["ansible.yaml"] = "yaml.ansible",
+  },
+  pattern = {
+    -- found these on: https://github.com/rafi/vim-config/blob/7ae731386c2f1d19f86ad446b2146d672ab9b463/filetype.lua#L31
+    [".*/playbooks/.*%.yaml"] = "yaml.ansible",
+    [".*/playbooks/.*%.yml"] = "yaml.ansible",
+    [".*/roles/.*%.yaml"] = "yaml.ansible",
+    [".*/roles/.*%.yml"] = "yaml.ansible",
+    [".*/inventory/.*%.ini"] = "ansible_hosts",
+  },
+})
