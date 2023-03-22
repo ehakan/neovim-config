ff43fe329acf66312f92152dd72e5dfc275acfb1
ehakan <ehakangencoglu@gmail.com>
2023-03-22 18:32:03 +0300

    unfold all automatically

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local unfold_all = vim.api.nvim_create_augroup("UnfoldAll", { clear = true })
+vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
+  group = unfold_all,
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_feedkeys("zR", "n", true)
+  end,
+})
