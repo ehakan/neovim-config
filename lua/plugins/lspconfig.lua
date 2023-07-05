c47f14586caa294a273c6827b346f33dec6c0c18
ehakan <ehakangencoglu@gmail.com>
2023-07-05 16:13:08 +0300

    disable incorrect kubernetes yaml detection

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,13 +3,7 @@ return {
     "nvim-lspconfig",
     opts = function(_, opts)
       opts.servers.yamlls = {
-        settings = {
-          yaml = {
-            schemas = {
-              kubernetes = "*.yaml",
-            },
-          },
-        },
+        settings = {},
       }
     end,
   },
