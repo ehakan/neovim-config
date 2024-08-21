73aeb1aec205dbbb0aaba8a681a82472e893ea75
ehakan <ehakangencoglu@gmail.com>
2024-08-21 17:49:50 +0300

    update packages

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,19 +4,15 @@ return {
     opts = function(_, opts)
       opts = {
         servers = {
-          yamlls = {
-            settings = {
-              yaml = {
-                hover = true,
-                completion = true,
-                validate = true,
-                schemaStore = {
-                  enable = false,
-                },
-                schemas = require("schemastore").yaml.schemas(),
-              },
-            },
-          },
+          -- yamlls = {
+          --   settings = {
+          --     yaml = {
+          --       schema = {
+          --         kubernetes = "kubernetes/**.yaml",
+          --       },
+          --     },
+          --   },
+          -- },
         },
       }
     end,
