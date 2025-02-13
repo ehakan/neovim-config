7532b27c1520395fa7bf8b940e0f153391edf5cc
ehakan <ehakangencoglu@gmail.com>
2025-02-13 17:50:16 +0300

    Checkpoint

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,5 +7,14 @@ return {
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    keys = {
+      {
+        "<leader>fo",
+        function()
+          require("oil").open_float()
+        end,
+        desc = "Float Oil",
+      },
+    },
   },
 }
