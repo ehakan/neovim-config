313b0a1074747ffad646eebd89a8ff29db06ba3f
ehakan <ehakangencoglu@gmail.com>
2026-03-04 21:21:02 +0300

    feat: checkpoint

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,40 +1,55 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
-  ft = "markdown",
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre /Users/ehakan/Library/Mobile Documents/iCloud~md~obsidian/Documents/ehakan/*.md",
-  --   "BufNewFile /Users/ehakan/Library/Mobile Documents/iCloud~md~obsidian/Documents/ehakan/*.md",
-  -- },
-  dependencies = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
+  {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*", -- use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Ensure blink.cmp is loaded before obsidian so that:
+    -- 1. completion.blink auto-detection succeeds
+    -- 2. register_providers() can call blink.add_source_provider()
+    dependencies = {
+      "saghen/blink.cmp",
+    },
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+      legacy_commands = false, -- will be removed in the next major release
 
-    -- see below for full list of optional dependencies 👇
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "ehakan",
-        path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/ehakan",
+      workspaces = {
+        {
+          name = "ehakan",
+          path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/ehakan",
+        },
+      },
+
+      frontmatter = {
+        enabled = false,
+      },
+
+      daily_notes = {
+        folder = "journal",
+        date_format = "YYYY-MM-DD",
+      },
+
+      ui = {
+        -- I'm using render-markdown.nvim already
+        enable = false,
       },
     },
+  },
 
-    disable_frontmatter = true,
-
-    daily_notes = {
-      folder = "journal",
-      date_format = "%Y-%m-%d",
-      template = nil,
-    },
-
-    ui = {
-      -- I'm using `MeanderingProgrammer/render-markdown.nvim` already
-      enable = false,
+  -- Tell blink to use obsidian sources for markdown files.
+  -- Obsidian registers the providers itself via blink.add_source_provider() during setup,
+  -- but its inject_sources() can miss the first buffer due to BufEnter timing,
+  -- so we ensure the source names are present in per_filetype from the start.
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        per_filetype = {
+          markdown = { inherit_defaults = true, "obsidian", "obsidian_new", "obsidian_tags" },
+        },
+      },
     },
   },
 }
